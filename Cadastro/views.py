e333fba325c2dd0b776516fff0dac9eda18e2781
Lucas <htslucasv@gmail.com>
2019-02-18 16:44:54 -0300

    test de fundo

--- Cadastro/views.py
+++ Cadastro/views.py
@@ -3,9 +3,11 @@ from random import randint
 import os
 # Create your views here.
 def home(request):
-    pasta_fundo = "../static/img/fundo"
+    """
+    pasta_fundo = "../static/img/fundo/"
     fundos = []
     for file in os.listdir(pasta_fundo):
         fundos.append(file)
     fundos = fundos[randint(0, 1)]
-    return render(request, "funcionalidades.html", {"fundo": fundos})
+    """
+    return render(request, "funcionalidades.html")
