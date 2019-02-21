a291bba54d21882f43d2638a42f95433123e414a
Lucas <htslucasv@gmail.com>
2019-02-21 09:47:35 -0300

    teste de modelo

--- Cadastro/settings.py
+++ Cadastro/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [u'cadastrofilme.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'Cadastro'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
