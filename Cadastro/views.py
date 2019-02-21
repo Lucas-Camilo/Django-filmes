6d28b4c9c8bc714fa252d24578133c74454d4989
Lucas <htslucasv@gmail.com>
2019-02-21 10:19:07 -0300

    teste de modelo

--- Cadastro/views.py
+++ Cadastro/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "funcionalidades.html")
-def visualizar(request):
-    return render(request, "vizualizar.html")
+
