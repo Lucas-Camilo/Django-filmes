3690c1022f35b800fb43b39bbb767659077545a1
Lucas <htslucasv@gmail.com>
2019-02-21 11:25:09 -0300

    teste de banco

--- funcoes/models.py
+++ funcoes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Filme(models.Model):
+    Nome = models.CharField(max_length=30)
+    Descricao = models.TextField()
+    Url = models.TextField()
